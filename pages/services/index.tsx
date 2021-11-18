2c0eaf8261ffb77ca0cbb6d5b036ae4ed9ba96d1
gmmurray <trinilime123@live.com>
2021-11-17 22:52:24 -0500

    finished services page

--- pages/services/index.tsx
+++ pages/services/index.tsx
@@ -1,14 +1,48 @@
-import clsx from 'clsx';
-import { NextPage } from 'next';
+import { Card, Col, Container, ListGroup, Row } from 'react-bootstrap';
+import { blockCn, elementCn } from '../../lib/helpers/bem';
+
 import { Fragment } from 'react';
-import { Card, Col, Container, Row } from 'react-bootstrap';
+import Image from 'next/image';
+import { NextPage } from 'next';
 import PageHeader from '../../components/shared/pageHeader/PageHeader';
 import { PageHeaderType } from '../../components/shared/pageHeader/pageHeaderType';
-import { blockCn, elementCn } from '../../lib/helpers/bem';
+import clsx from 'clsx';
 
 const contentCn = blockCn('content');
 const cardCn = elementCn(contentCn, 'card');
 const imageCn = elementCn(cardCn, 'responsive-rounded-start-image');
+type ServiceCard = {
+    title: string;
+    subtitle: string;
+    image: string;
+    points: string[];
+};
+const serviceCards: ServiceCard[] = [
+    {
+        title: 'Development',
+        subtitle:
+            'The first step is to build a website that support your needs as a small business',
+        image: '/images/development.jpg',
+        points: [
+            'Single page web fronts',
+            'Portfolio-style websites for creatives',
+            'Multi-page websites like this one',
+            'More complicated web applications that assist business processes',
+            'Websites created using third party sites such as Wordpress, Squarespace, etc.',
+        ],
+    },
+    {
+        title: 'Support',
+        subtitle:
+            'After development is complete, I can assist with keeping the website up to date',
+        image: '/images/support.jpg',
+        points: [
+            'Adding new content',
+            'Updating existing content as necessary',
+            'Resolving any issues that may come up. Unlikely, but still good to be prepared',
+        ],
+    },
+];
 
 const Services: NextPage = () => {
     return (
@@ -20,7 +54,56 @@ const Services: NextPage = () => {
             />
             <Container>
                 <Row>
-                    <Col>
+                    {serviceCards.map(
+                        ({ title, subtitle, image, points }, index) => (
+                            <Col key={`card-${index}`} xs={12}>
+                                <Card
+                                    className={clsx(
+                                        cardCn,
+                                        index === 1 ? 'my-4' : 'mt-4',
+                                    )}
+                                >
+                                    <Row>
+                                        <Col xs={12} md={5}>
+                                            <Image
+                                                height="800"
+                                                width="1200"
+                                                layout="responsive"
+                                                src={image}
+                                                alt={`${title} image`}
+                                                className={clsx(
+                                                    'img-fluid',
+                                                    imageCn,
+                                                )}
+                                            />
+                                        </Col>
+                                        <Col xs={12} md={7}>
+                                            <Card.Body>
+                                                <Card.Text>
+                                                    <h2 className="display-5">
+                                                        {title}
+                                                    </h2>
+                                                    <h5>{subtitle}</h5>
+                                                    <ListGroup variant="flush">
+                                                        {points.map(
+                                                            (point, index) => (
+                                                                <ListGroup.Item
+                                                                    key={index}
+                                                                >
+                                                                    {point}
+                                                                </ListGroup.Item>
+                                                            ),
+                                                        )}
+                                                    </ListGroup>
+                                                </Card.Text>
+                                            </Card.Body>
+                                        </Col>
+                                    </Row>
+                                </Card>
+                            </Col>
+                        ),
+                    )}
+                    {/* <Col>
                         <Card className={clsx(cardCn, 'mt-4')}>
                             <Row>
                                 <Col xs={12} md={4}>
@@ -46,7 +129,7 @@ const Services: NextPage = () => {
                                 </Col>
                             </Row>
                         </Card>
-                    </Col>
+                    </Col> */}
                 </Row>
             </Container>
         </Fragment>
